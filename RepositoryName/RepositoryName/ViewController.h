e0778b27118c4b66595b8ba3e2775bf5b33e2488
张绍康 <mac@MacdeMacBook-Pro.local>
2016-03-12 16:39:15 +0800

    上传
    上传

---
+++ RepositoryName/RepositoryName/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RepositoryName
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RepositoryName/RepositoryName/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RepositoryName
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+//    NSLog(@"Hello  World");
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
